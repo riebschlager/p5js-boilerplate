af97f6bfef7019582ecc67f9b0a065263ffe0c1f
Chris Riebschlager <riebschlager@gmail.com>
2016-01-30 17:38:19 -0600

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# p5.js Boilerplate
+
+A set of reasonable defaults to kick off your [p5js](http://p5js.org/) project.
---
+++ assets/js/sketch.js
@@ -0,0 +1,19 @@
+var sketch = function(p) {
+
+  var canvas;
+  var sketchWidth = window.innerWidth;
+  var sketchHeight = window.innerHeight;
+
+  p.setup = function() {
+    var canvas = p.createCanvas(sketchWidth, sketchHeight);
+    canvas.parent('sketch');
+  };
+
+  p.draw = function() {
+    p.background(0, 10);
+    p.rect(p.random(p.width), p.random(p.height), 10, 10);
+  };
+
+};
+
+var myp5 = new p5(sketch);
