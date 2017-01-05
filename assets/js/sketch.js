c6d3a03d9b7e3901f3ce1419748eafb336f6b9ea
Chris Riebschlager <riebschlager@gmail.com>
2017-01-04 21:02:45 -0600

    clearing cobwebs

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,18 +1,18 @@
 var sketch = function(p) {
 
-  var canvas;
-  var sketchWidth = window.innerWidth;
-  var sketchHeight = window.innerHeight;
+    var canvas;
+    var sketchWidth = window.innerWidth;
+    var sketchHeight = window.innerHeight;
 
-  p.setup = function() {
-    var canvas = p.createCanvas(sketchWidth, sketchHeight);
-    canvas.parent('sketch');
-  };
+    p.setup = function() {
+        var canvas = p.createCanvas(sketchWidth, sketchHeight);
+        canvas.parent('sketch');
+    };
 
-  p.draw = function() {
-    p.background(0, 10);
-    p.rect(p.random(p.width), p.random(p.height), 10, 10);
-  };
+    p.draw = function() {
+        p.background(0, 10);
+        p.rect(p.random(p.width), p.random(p.height), 10, 10);
+    };
 
 };
 
